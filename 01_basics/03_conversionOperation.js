542c759b9ee96bbaaaadf5fa42c31c63d251814d
Saurabh Kuntal <wintersoldier110@gmail.com>
2024-11-20 15:59:11 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,9 +1,10 @@
-// let score = "33abc"
+let score = "33abc"
 // console.log(typeof(score))
 
-// let valueInNumber = Number(score)
+let valueInNumber = Number(score)
 // console.log(typeof(valueInNumber))
 // console.log(valueInNumber)
+
 //"33" => number
 //"33abc" => NaN
 // true => 1
@@ -12,8 +13,30 @@
 
 let isLoggedIn = 1;
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn)
+
 // number => true
 //0 => false 
 //string => true
 //empty string "" => false
+
+//------------operations------------//
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2*2);
+// console.log(2+2);
+// console.log(3**3);
+// console.log(3/2);
+// console.log(3%2);
+
+let str1 = "hello"
+let str2 = " Saurabh"
+let str3 = str1 + str2
+// console.log(str3);
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
