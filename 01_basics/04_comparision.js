9656ebf039c1db23fefbb9b421d4874a1062c88d
Saurabh Kuntal <wintersoldier110@gmail.com>
2024-11-20 16:09:16 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,13 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1);
+// console.log("02">1);
+//console.log(null > 0); // false 
+//console.log(null == 0); // false
+//console.log(null >= 0); // true
+
+//in JS equality == and comparision >,<,>=,<= operator work differently, comparision convert null to a number and treat it as 0 thus in null>=0 it give true but in null>0 it give false.
