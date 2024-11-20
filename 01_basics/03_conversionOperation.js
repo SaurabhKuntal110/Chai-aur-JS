c1b18354865c02c0b032a7b2533cdb1d58e0c1a5
Saurabh Kuntal <wintersoldier110@gmail.com>
2024-11-20 08:21:04 +0000

    Conversion of Datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+// let score = "33abc"
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+//"33" => number
+//"33abc" => NaN
+// true => 1
+// false => 0
+//null => 0
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+// number => true
+//0 => false 
+//string => true
+//empty string "" => false
