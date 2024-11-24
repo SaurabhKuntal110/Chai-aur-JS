d428832334e5e46546e3666db5f88739bfbf0fa1
Saurabh Kuntal <wintersoldier110@gmail.com>
2024-11-24 08:35:43 +0000

    taking parameter in functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -31,4 +31,35 @@ function userLoginMessage(user){
     }
 }
 
-console.log(userLoginMessage("Saurabh Kuntal"));
+//console.log(userLoginMessage("Saurabh Kuntal"));
+
+
+function calculateCartPrice(...num1){
+    return num1
+}
+
+//console.log(calculateCartPrice(200,300,400))
+
+const user = {
+    userName: "Saurabh",
+    price: 150
+}
+
+function handleObject(anyObject){
+    console.log(`user name is ${anyObject.userName} and price is ${anyObject.price}`);
+}
+//handleObject(user)
+handleObject({
+    userName:"Sam",
+    price: 200
+})
+
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
