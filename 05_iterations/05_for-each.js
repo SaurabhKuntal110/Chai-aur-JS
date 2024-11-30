1df2622a44f5c8c3c74a2d88ef26a5c0c3285c27
Saurabh Kuntal <wintersoldier110@gmail.com>
2024-11-30 10:03:46 +0000

    for each

---
+++ 05_iterations/05_for-each.js
@@ -0,0 +1,42 @@
+
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// forEach requires a callback function which is a normal func with no name and value will be taken as parameter
+
+// coding.forEach( function (val){
+//     console.log(val);
+// } )
+
+// coding.forEach( (item) => {
+//     console.log(item);
+// } )
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)  // callback func reference is given but not executed
+
+// coding.forEach( (item, index, arr)=> {
+//     console.log(item, index, arr);
+// } )
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+
+myCoding.forEach( (item) => {
+    
+    console.log(item.languageName);
+} )
